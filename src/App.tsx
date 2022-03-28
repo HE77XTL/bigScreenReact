1bc2e06e45f28e35c1007fe4a9b22f3227879de7
何东升 <he77505357@163.com>
2022-03-28 17:38:29 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.less';
+import * as React from "react";
+
+function App() {
+    return (
+        <div className="App">
+            大屏
+            <div className="textBox">
+                textBox
+            </div>
+        </div>
+    );
+}
+
+export default App;
